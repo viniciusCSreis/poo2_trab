993431624585d728a674aadfe7f6b31bab2a594c
Maycon Pacheco <mayconjrpacheco@Maycons-MacBook-Pro.local>
2018-12-04 01:26:19 -0200

    Fix merge

--- desktop/src/br/ufu/poo2/desktop/DesktopLauncher.java
+++ desktop/src/br/ufu/poo2/desktop/DesktopLauncher.java
@@ -7,6 +7,9 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "SpaceJ";
+		config.width = 1366;
+		config.height = 768;
 		new LwjglApplication(new Poo2Game(), config);
 	}
 }
